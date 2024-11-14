45aef37381f23436f0ff0908bf1c83147ea01bd2
Herbie James <herbiejames.dm@gmail.com>
2024-11-14 13:20:27 +0000

    add level generation

--- frogger/assets/js/script.js
+++ frogger/assets/js/script.js
@@ -33,8 +33,8 @@ function initSprite(x, y, src, classes, id) {
     img.style.gridColumn = x;
     img.style.gridRow    = y;
     src = "/frogger/assets/images/".concat(src);
-    img.src = img.src != null ? src : null;
-    img.id  = img.id  != null ? id  : null;
+    img.src = img.src !== undefined ? src : undefined; // doesn't work
+    img.id  = img.id  !== undefined ? id  : undefined; // doesn't work
     var neighbor;
     for (let i = 0; i <= grid.childElementCount; i++) {
         var head = document.querySelector(`#game-display :nth-child(${i})`);
@@ -57,9 +57,73 @@ function initSprite(x, y, src, classes, id) {
     } else {
         grid.appendChild(img);
     }
-    if (img.id != null) {
+    if (id  !== undefined) {
         console.log('initSprite(): "#'+id, "created before", neighbor, "at", headX, headY+'"')
     }
+}
+
+function initRoadRows(y) {
+    console.log("y: ", y)
+    y.forEach((element) => {
+        if (y.includes(element-1)) {
+            if (y.includes(element+1)) {
+                for (let i = 0; i < 12; i++) {
+                    initSprite(i, element, "road4.PNG", ["tile", "road"]);
+                }
+            } else {
+                for (let i = 0; i < 12; i++) {
+                    initSprite(i, element, "road3.PNG", ["tile", "road"]);
+                }
+            }
+        } else if (y.includes(element+1)) {
+            for (let i = 0; i < 12; i++) {
+                initSprite(i, element, "road2.PNG", ["tile", "road"]);
+            }
+        } else {
+            for (let i = 0; i < 12; i++) {
+                initSprite(i, element, "road1.PNG", ["tile", "road"]);
+            }
+        }
+    })
+}
+
+function initWaterRow(y) {
+    for (let i = 0; i < 12; i++) {
+        initSprite(i, y, "water1.PNG", ["tile", "water"]);
+    }
+}
+
+function initGrassRow(y) {
+    for (let i = 0; i < 12; i++) {
+        initSprite(i, y, "grass1.PNG", ["tile", "grass"]);
+    }
+}
+
+function initLevel(y) {
+    var roadRows = []
+    for (let i = 0; i < 12; i++) {
+        initSprite(i, 11, "grass3.PNG", ["tile"]);
+    }
+    for (let i = 0; i < 12; i++) {
+        initSprite(i, 1, "grass4.PNG", ["tile"]);
+    }
+    for (let y = 2; y < 11; y++) {
+        var roadRow  = 9 - roadRows.length;
+        var waterRow = 9 - (document.querySelectorAll('#game-display .water').length)/12;
+        var grassRow = 9 - (document.querySelectorAll('#game-display .water').length)/12;
+        var totalRow = roadRow + waterRow + grassRow
+        if (totalRow * Math.random() <= roadRow) {
+            roadRows.push(y)
+            console.log("ROAD");
+        } else if (totalRow * Math.random() <= roadRow + waterRow) {
+            initWaterRow(y);
+            console.log("WATER");
+        } else {
+            initGrassRow(y);
+            console.log("GRASS");
+        }
+    }
+    initRoadRows(roadRows)
 }
 
 function startUp() {
@@ -70,7 +134,8 @@ function startUp() {
             // remove startBtn
             // remove any button press event listener
 
-        initSprite(6, 12, frog, ["entity"], "player");
+        initSprite(6, 11, frog, ["entity"], "player");
+        initLevel();
     }
 }
 
