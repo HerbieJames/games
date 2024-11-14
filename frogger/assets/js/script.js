eb76dd0343a88fffcd065ab8fbc86c1fa0d478ea
Herbie James <herbiejames.dm@gmail.com>
2024-11-14 17:43:35 +0000

    frog goes now

--- frogger/assets/js/script.js
+++ frogger/assets/js/script.js
@@ -1,20 +1,18 @@
+//DECLARATIONS
+const startBtnEl = document.getElementById("startBtnEl");
+const upBtnEl    = document.getElementById("btnUpEl");
+const leftBtnEl  = document.getElementById("btnLeftEl");
+const rightBtnEl = document.getElementById("btnRightEl");
+const downBtnEl  = document.getElementById("btnDownEl");
+const grid       = document.getElementById("gameDisplayEl");
+let frog   = "frog1.PNG";
 let active = false;
-let startBtn = document.getElementById("startBtn");
-let grid = document.getElementById("game-display");
-let tick = 0
-let frog = "frog1.PNG"
+let tick   = 0;
+let player;
+let playerX;
+let playerY;
 
-function timer(){
-    if (tick == 1) {
-        // console.log(tick)
-        if (active == false){
-            startBtn.innerHTML = startBtn.innerHTML == "" ? "START" : "";
-        }
-    }
-    tick += tick == 4 ? (-3) : 1
-    setTimeout(timer, 250);
-}
-
+//INITIALIZATIONS
 /**Creates an image with src, id and class 
  * at a specified location on the grid, and
  * appropriate position in the DOM
@@ -24,46 +22,46 @@ function timer(){
  * @param {Array}  classes Array of strings; classes for image to assume
  * @param {String} id      id for image to assume
  */
-
 function initSprite(x, y, src, classes, id) {
     var img = document.createElement('img');
-    img.classList.add("sprite");
-    img.alt = "";
-    img.style.imageRendering = "pixelated";
-    img.style.gridColumn = x;
-    img.style.gridRow    = y;
-    src = "/frogger/assets/images/".concat(src);
-    img.src = img.src !== undefined ? src : undefined; // doesn't work
-    img.id  = img.id  !== undefined ? id  : undefined; // doesn't work
     var neighbor;
     for (let i = 0; i <= grid.childElementCount; i++) {
-        var head = document.querySelector(`#game-display :nth-child(${i})`);
-        if (head != null) {
-            var headX = head.style.gridColumn
-            var headY = head.style.gridRow
+        var head = document.querySelector(`#gameDisplayEl :nth-child(${i})`);
+        if (head != undefined) {
+            var headX = head.style.gridColumn;
+            var headY = head.style.gridRow;
             if (((headX >= x)&&(headY == y))||(headY > y)) {
                 neighbor = head;
                 break;
             }
         }
     }
-    if (classes != null) { 
-        classes.forEach((element) => {
-            img.classList.add(element);
-        });
+    img.classList.add("sprite");
+    img.alt = " ";
+    img.style.gridColumn = x;
+    img.style.gridRow    = y;
+    if (src != undefined) { 
+        src = "/frogger/assets/images/".concat(src);
+        img.src = src;
     }
-    if (neighbor != null) {
-        neighbor.insertAdjacentElement("beforebegin", img)
+    if (classes != undefined) { 
+        classes.forEach((element) => { img.classList.add(element); });
+    }
+    if (id  !== undefined) { img.id  = id; }
+    if (neighbor != undefined) {
+        neighbor.insertAdjacentElement("beforebegin", img);
     } else {
         grid.appendChild(img);
     }
     if (id  !== undefined) {
-        console.log('initSprite(): "#'+id, "created before", neighbor, "at", headX, headY+'"')
+        console.log('initSprite(): "#'+id, "created before", neighbor, "at", headX, headY+'"');
     }
 }
 
+/**Initializes road tiles on all rows specified by an array.
+ * @param {Array} y the rows where tiles will be generated
+ */
 function initRoadRows(y) {
-    console.log("y: ", y)
     y.forEach((element) => {
         if (y.includes(element-1)) {
             if (y.includes(element+1)) {
@@ -87,19 +85,32 @@ function initRoadRows(y) {
     })
 }
 
+/**Initializes water tiles on the row specified.
+ * @param {Number} y the row where tiles will be generated
+ */
 function initWaterRow(y) {
     for (let i = 1; i <= 12; i++) {
         initSprite(i, y, "water1.PNG", ["tile", "water"]);
     }
 }
 
+/**Initializes water tiles on the row specified.
+ * @param {Number} y the row where tiles will be generated
+ */
 function initGrassRow(y) {
     for (let i = 1; i <= 12; i++) {
         initSprite(i, y, "grass1.PNG", ["tile", "grass"]);
     }
 }
 
-function initLevel(y) {
+/**Initializes level by randomly allocating certain
+ * types of tiles to certain rows, then initializing them.
+ */
+function initLevel() {
+    if (player) {
+        player.gridColumn = 6;
+        player.gridColumn = 11;
+    }
     var roadRows = []
     for (let i = 1; i <= 12; i++) {
         initSprite(i, 11, "grass3.PNG", ["tile"]);
@@ -109,11 +120,11 @@ function initLevel(y) {
     }
     for (let y = 2; y < 11; y++) {
         var roadRow  = 9 - roadRows.length;
-        var waterRow = 9 - (document.querySelectorAll('#game-display .water').length)/12;
-        var grassRow = 9 - (document.querySelectorAll('#game-display .water').length)/12;
-        var totalRow = roadRow + waterRow + grassRow
+        var waterRow = 9 - (document.querySelectorAll('#gameDisplayEl .water').length)/12;
+        var grassRow = 9 - (document.querySelectorAll('#gameDisplayEl .water').length)/12;
+        var totalRow = roadRow + waterRow + grassRow;
         if (totalRow * Math.random() <= roadRow) {
-            roadRows.push(y)
+            roadRows.push(y);
             console.log("ROAD");
         } else if (totalRow * Math.random() <= roadRow + waterRow) {
             initWaterRow(y);
@@ -123,20 +134,47 @@ function initLevel(y) {
             console.log("GRASS");
         }
     }
-    initRoadRows(roadRows)
+    initRoadRows(roadRows);
 }
 
+//INTERACTIONS
+/**Repeats commands distributed in half and quarters
+ */
+function delta(){
+    if (tick % 2 == 1) {
+        if (tick == 1) {
+            startBtnEl.innerHTML = startBtnEl.innerHTML == "" ? "START" : "";
+
+        } else { // 3
+
+        }
+    } else {
+        if (tick == 2) {
+
+        } else { // 4
+        
+        }
+    }
+    tick += tick == 4 ? (-3) : 1;
+    setTimeout(delta, 250);
+}
+/** Initializes Frogger and enables controls
+ */
 function startUp() {
     if (active == false) {
         active = true;
-        startBtn.style.display = "none";
-        grid.style.backgroundColor = "black"
-            // remove startBtn
-            // remove any button press event listener
-
-        initSprite(6, 11, frog, ["entity"], "player");
+        startBtnEl.style.display = "none";
         initLevel();
+        initSprite(1, 1, frog, ["entity"], "player");
+        player = document.getElementById("player");
+        player.style.gridColumn = 6;
+        player.style.gridRow = 11;
+        console.log("player:", player)
     }
 }
 
-timer();
+delta();
+upBtnEl.addEventListener("click", () => {
+    console.log("Up!", typeof(playerY));
+    player.style.gridRow -= 1; // :)
+})
